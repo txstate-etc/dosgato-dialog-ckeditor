2ac85a5de2a7ef37a5782468e74ceebc0dffe4a5
Nick Wing <nwing@txstate.edu>
2023-01-06 09:48:19 -0600

    bring the demo API up to date with latest chooser changes

--- src/demo/DemoChooserAPI.ts
+++ src/demo/DemoChooserAPI.ts
@@ -1,64 +1,74 @@
-import type { AnyUIItem, Asset, ChooserType, Client, Folder, Page, Source } from '@dosgato/dialog'
-import { filterAsync, randomid } from 'txstate-utils'
+import type { Asset, ChooserType, Client, Folder, Page, Source, AnyItem } from '@dosgato/dialog'
+import { randomid } from 'txstate-utils'
 
+interface StoredAsset extends Omit<Asset, 'source'> {}
 interface RootFolder {
-  children?: (Asset|FolderWithChildren)[]
+  children?: (StoredAsset | FolderWithChildren)[]
   acceptsUpload?: boolean
 }
 interface RootPage {
   children?: PageWithChildren[]
 }
-interface FolderWithChildren extends Folder {
-  children?: (Asset|FolderWithChildren)[]
+interface FolderWithChildren extends Omit<Folder, 'source'> {
+  children?: (StoredAsset | FolderWithChildren)[]
 }
-interface PageWithChildren extends Page {
+interface PageWithChildren extends Omit<Page, 'source'> {
   children?: PageWithChildren[]
 }
 
-type AnyItem = Asset|FolderWithChildren|PageWithChildren
+type AnyStoredItem = StoredAsset | FolderWithChildren | PageWithChildren
 
-const assets: Record<string, RootFolder|RootPage> = {
+const assets: Record<string, RootFolder | RootPage> = {
   Assets: {
     children: [
       {
         type: 'folder',
         id: 'folder-1',
         name: 'biology',
-        path: '/',
+        path: '/biology',
+        url: '/assets/biology',
         acceptsUpload: true,
+        hasChildren: true,
         children: [
           {
             type: 'folder',
             id: 'folder-4',
             name: 'evolutionary',
-            path: '/biology',
+            path: '/biology/evolutionary',
+            url: '/assets/biology/evolutionary',
             acceptsUpload: false,
+            hasChildren: true,
             children: [
-              { type: 'asset', id: 'asset-3', path: '/biology/evolutionary', name: 'missinglink.png', mime: 'image/png', bytes: 196672, url: '/demo-full.png', image: { width: 909, height: 1114, thumbnailUrl: '/demo-thumb.png' } }
+              { type: 'asset', id: 'asset-3', path: '/biology/evolutionary/missinglink.png', name: 'missinglink.png', mime: 'image/png', bytes: 196672, url: '/demo-full.png', image: { width: 909, height: 1114, thumbnailUrl: '/demo-thumb.png' } }
             ]
           },
-          { type: 'folder', id: 'folder-5', name: 'humananatomy', path: '/biology', acceptsUpload: false }
+          { type: 'folder', id: 'folder-5', name: 'humananatomy', path: '/biology/humananatomy', url: '/assets/biology/humananatomy', acceptsUpload: false, hasChildren: false }
         ]
       },
       {
         type: 'folder',
         id: 'folder-2',
         name: 'chemistry',
-        path: '/',
+        path: '/chemistry',
+        url: '/assets/chemistry',
         acceptsUpload: true,
+        hasChildren: true,
         children: [
-          { type: 'folder', id: 'folder-6', name: 'organic', path: '/chemistry', acceptsUpload: true }
+          { type: 'folder', id: 'folder-6', name: 'organic', path: '/chemistry/organic', url: '/assets/chemistry/organic', acceptsUpload: true, hasChildren: false }
         ]
       },
       {
         type: 'folder',
         id: 'folder-3',
         name: 'physics',
-        path: '/',
+        path: '/physics',
+        url: '/assets/physics',
         acceptsUpload: true,
+        hasChildren: true,
         children: [
-          { type: 'asset', id: 'asset-1', path: '/physics', name: 'cannondiagram.png', mime: 'image/png', bytes: 196672, url: '/demo-full.png', image: { width: 909, height: 1114, thumbnailUrl: '/demo-thumb.png' } },
-          { type: 'asset', id: 'asset-2', path: '/physics', name: 'modernphysics.pdf', mime: 'application/pdf', bytes: 1264, url: '/blankpdf.pdf' }
+          { type: 'asset', id: 'asset-1', path: '/physics/cannondiagram.png', name: 'cannondiagram.png', mime: 'image/png', bytes: 196672, url: '/demo-full.png', image: { width: 909, height: 1114, thumbnailUrl: '/demo-thumb.png' } },
+          { type: 'asset', id: 'asset-2', path: '/physics/modernphysics.pdf', name: 'modernphysics.pdf', mime: 'application/pdf', bytes: 1264, url: '/blankpdf.pdf' },
+          { type: 'asset', id: 'asset-3', path: '/physics/bobcat.jpg', name: 'bobcat.jpg', mime: 'image/jpeg', bytes: 3793056, url: '/bobcat.jpg', image: { width: 6016, height: 4016, thumbnailUrl: '/bobcat-thumbnail.jpg' } }
         ]
       }
     ]
@@ -71,52 +81,58 @@ const assets: Record<string, RootFolder|RootPage> = {
       {
         type: 'page',
         id: 'page-1',
-        path: '/',
+        path: '/human-resources',
         name: 'human-resources',
         url: 'https://example.org/human-resources.html',
         title: 'Human Resources',
+        hasChildren: true,
         children: [
           {
             type: 'page',
             id: 'page-5',
-            path: '/human-resources',
+            path: '/human-resources/about',
             name: 'about',
             url: 'https://example.org/human-resources/about.html',
-            title: 'About Us'
+            title: 'About Us',
+            hasChildren: false
           },
           {
             type: 'page',
             id: 'page-6',
-            path: '/human-resources',
+            path: '/human-resources/resources',
             name: 'resources',
             url: 'https://example.org/human-resources/resources.html',
-            title: 'Forms & Other Resources'
+            title: 'Forms & Other Resources',
+            hasChildren: false
           }
         ]
       },
       {
         type: 'page',
         id: 'page-2',
-        path: '/',
+        path: '/history',
         name: 'history',
         url: 'https://example.org/history.html',
-        title: 'Department of History'
+        title: 'Department of History',
+        hasChildren: false
       },
       {
         type: 'page',
         id: 'page-3',
-        path: '/',
+        path: '/math',
         name: 'math',
         url: 'https://example.org/math.html',
-        title: 'Department of Mathematics'
+        title: 'Department of Mathematics',
+        hasChildren: false
       },
       {
         type: 'page',
         id: 'page-4',
-        path: '/',
+        path: '/vpit',
         name: 'vpit',
         url: 'https://example.org/vpit.html',
-        title: 'Information Technology'
+        title: 'Information Technology',
+        hasChildren: false
       }
     ]
   }
@@ -128,26 +144,26 @@ class DemoChooserAPI implements Client {
     return [{ type: 'page', name: 'Pages' }] as Source[]
   }
 
-  findFolder (source: string, path: string): RootFolder|RootPage|FolderWithChildren|PageWithChildren {
+  findFolder (source: string, path: string): RootFolder | RootPage | FolderWithChildren | PageWithChildren {
     if (path === '/') return assets[source]
     const parts = path.substring(1).split('/')
     let folders = assets[source].children
-    let folder: AnyItem
+    let folder: AnyStoredItem | undefined
     for (const part of parts) {
-      folder = (folders as AnyItem[]).find(f => f.name === part)
+      folder = (folders as AnyStoredItem[]).find(f => f.name === part)
       if (!folder) throw new Error(`path ${path} not found in source ${source}`)
       if (folder.type === 'asset') throw new Error(`path ${path} refers to an asset but expected a folder`)
       folders = folder.children ?? []
     }
-    return folder as FolderWithChildren|PageWithChildren
+    return folder as FolderWithChildren | PageWithChildren
   }
 
-  collectItems (item: Asset|FolderWithChildren|PageWithChildren|RootPage|RootFolder) {
-    const ret = []
-    if ('type' in item) ret.push(item)
-    if ('children' in item && item.children.length) {
+  collectItems (item: StoredAsset | FolderWithChildren | PageWithChildren | RootPage | RootFolder, source: string): AnyItem[] {
+    const ret: AnyItem[] = []
+    if ('type' in item) ret.push({ ...item, source })
+    if ('children' in item && item.children?.length) {
       for (const f of item.children) {
-        ret.push(...this.collectItems(f))
+        ret.push(...this.collectItems(f, source))
       }
     }
     return ret
@@ -155,39 +171,39 @@ class DemoChooserAPI implements Client {
 
   async getChildren (source: string, path: string) {
     const folder = this.findFolder(source, path)
-    return folder.children as AnyItem[] ?? []
+    return folder.children?.map(c => ({ ...c, source })) as AnyItem[] ?? []
   }
 
   async find (source: string, path: string, searchstring: string) {
     const folder = this.findFolder(source, path)
-    const items = this.collectItems(folder)
+    const items = this.collectItems(folder, source)
     const search = searchstring.toLocaleLowerCase()
     return items.filter(a => a.name.toLocaleLowerCase().includes(search))
   }
 
-  async findById (id: string) {
-    for (const rootfolder of Object.values(assets)) {
-      const found = this.collectItems(rootfolder).find(a => a.id === id)
+  async findById (id: string): Promise<AnyItem | undefined> {
+    for (const [key, rootfolder] of Object.entries(assets)) {
+      const found = this.collectItems(rootfolder, key).find(a => a.id === id)
       if (found) return found
     }
-    throw new Error('Asset or Folder ID not found.')
+    return undefined
   }
 
   async findByUrl (url: string) {
-    for (const rootfolder of Object.values(assets)) {
-      const found = this.collectItems(rootfolder).find(a => a.url === url)
+    for (const [source, rootfolder] of Object.entries(assets)) {
+      const found = this.collectItems(rootfolder, source).find(a => 'url' in a && a.url === url)
       if (found) return found
     }
-    throw new Error('Asset or Folder ID not found.')
+    return undefined
   }
 
   async upload (source: string, path: string, files: FileList) {
-    const folder = this.findFolder(source, path) as RootFolder|FolderWithChildren
+    const folder = this.findFolder(source, path) as RootFolder | FolderWithChildren
     if (!folder?.acceptsUpload) throw new Error('User may not upload to this folder')
     folder.children ??= []
     for (const file of Array.from(files)) {
       const isImage = file.type.startsWith('image')
-      const asset: Asset = { type: 'asset', id: randomid(), path, name: file.name, mime: isImage ? 'image/png' : 'application/pdf', bytes: isImage ? 196672 : 1264, url: isImage ? '/demo-full.png' : '/blankpdf.pdf' }
+      const asset: StoredAsset = { type: 'asset', id: randomid(), path, name: file.name, mime: isImage ? 'image/png' : 'application/pdf', bytes: isImage ? 196672 : 1264, url: isImage ? '/static/demo-full.png' : '/static/blankpdf.pdf' }
       if (isImage) {
         asset.image = {
           width: 909,
