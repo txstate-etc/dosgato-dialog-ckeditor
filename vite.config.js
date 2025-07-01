1fa2f8b56c370df8d771a7b0031c68837ad10830
Nick Wing <nwing@txstate.edu>
2025-07-01 10:38:01 -0500

    update to latest versions and support svelte 5, release 1.1.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-        plugins: [sveltekit()]
-};
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    exclude: ['@iconify-icons/ph', '@iconify-icons/mdi'] // exclude iconify libraries to preserve HMR
+  }
+}
 
 export default config;
